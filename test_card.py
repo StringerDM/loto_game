e8a2b2a01622bc9ae5eb9030b90a26d281c37fd1
StringerDM <103386227+StringerDM@users.noreply.github.com>
2023-03-15 16:38:25 +0300

    HW10 add unittests

---
+++ test_card.py
@@ -0,0 +1,46 @@
+import unittest
+import io
+import sys
+
+from card import Card
+
+
+class TestCard(unittest.TestCase):
+
+    def test_init(self):
+        card = Card('Карта игрока')
+        self.assertEqual(card.name, 'Карта игрока')
+        self.assertListEqual(card.cals, [0, 1, 2, 3, 4, 5, 6, 7, 8])
+        self.assertEqual(card.barrels_in_row, 5)
+        self.assertEqual(card.rows, 3)
+        self.assertTrue(len(card.get_card()) > 0)
+
+    def test_get_card(self):
+        card = Card('Карта игрока').get_card()
+        self.assertEqual(len(card), 3)
+        for row in card:
+            self.assertEqual(len(row), 9)
+            self.assertEqual(len([i for i in row if i > 0]), 5)
+
+    def test_print_card(self):
+        card = Card('Карта игрока')
+        card.card = [[0, 63, 0, 7, 4, 18, 21, 0, 0], [0, 0, 0, 28, 15, 0, 44, 65, 51],
+                     [0, 69, 0, 67, 89, 0, 81, 27, 0]]
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        card.print_card()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue(), '-------Карта игрока-------\n'
+                                                     '   63     7  4 18 21       \n'
+                                                     '         28 15    44 65 51 \n'
+                                                     '   69    67 89    81 27    \n'
+                                                     '--------------------------\n')
+
+    def test_try_number(self):
+        card = Card('Карта игрока')
+        card.card = [[0, 63, 0, 7, 4, 18, 21, 0, 0], [0, 0, 0, 28, 15, 0, 44, 65, 51],
+                     [0, 69, 0, 67, 89, 0, 81, 27, 0]]
+        self.assertTrue(card.try_number(63))
+        self.assertTrue(card.try_number(28))
+        self.assertTrue(card.try_number(89))
